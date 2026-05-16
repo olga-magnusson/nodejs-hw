f6455bda1c141a30b54e488956f85573c0dd041b
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-05-16 22:58:53 +0200

    change port to 3000

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import pinoHttp from 'pino-http';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
