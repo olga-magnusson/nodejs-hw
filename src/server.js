50f66aea5281bcc7e34c94a8ed6c8f6398ef791e
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-05-16 22:51:28 +0200

    change server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const pinoHttp =require('pino-http');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
