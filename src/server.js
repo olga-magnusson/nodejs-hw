e07c5c81efe1b60355fb68192f47ed627b0b5fb1
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-05-16 21:36:19 +0200

    change pino to pinoHttp

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const pino =require('pino');
+const pinoHttp =require('pino-http');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(pinoHttp());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
