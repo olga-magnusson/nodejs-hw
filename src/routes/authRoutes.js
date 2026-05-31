49f8d066cb65680ccca772ac536c079c6be3504b
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-01 01:20:03 +0200

    created possible to refresh session

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser, logoutUser } from '../controllers/authController';
+import { registerUser, loginUser, logoutUser, refreshUserSession } from '../controllers/authController';
 import { registerUserSchema, loginUserSchema } from '../validations/authValidation';
 
 const router = Router();
@@ -9,5 +9,6 @@ router.post('/auth/register', celebrate(registerUserSchema),
 registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
