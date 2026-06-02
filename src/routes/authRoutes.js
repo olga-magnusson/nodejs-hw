c501480fd515baeeb8db3775f7a43cf090281de3
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-02 23:27:33 +0200

    creat possibility to change password by mail

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser, logoutUser, refreshUserSession } from '../controllers/authController.js';
-import { registerUserSchema, loginUserSchema } from '../validations/authValidation.js';
+import { registerUser, loginUser, logoutUser, refreshUserSession, requestResetEmail } from '../controllers/authController.js';
+import { registerUserSchema, loginUserSchema, requestResetEmailSchema } from '../validations/authValidation.js';
+import { resetPassword } from '../controllers/authController.js';
+import { resetPasswordSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
@@ -10,5 +12,7 @@ registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
