e83fc922da4ed2f7486558dccc6860c325d09057
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-03 23:01:40 +0200

    fixed updated avatar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,11 +10,11 @@ export const updateUserAvatar = async (req, res) => {
   }
 
   const result = await saveFileToCloudinary(file.buffer, user._id);
-  await User.findOneAndUpdate(
+  const updatedUser = await User.findOneAndUpdate(
     {_id: user._id},
     {avatar: result.secure_url},
     {returnDocument: "after"},
   );
 
-  res.status(200).json({url: user.avatar});
+  res.status(200).json({url: updatedUser.avatar});
 };
