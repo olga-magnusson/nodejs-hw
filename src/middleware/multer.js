6e42ab25e81fcdf2a20d6c23f292c16920188fc1
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-03 23:11:04 +0200

    change allowd mimetype

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -4,9 +4,7 @@ export const upload = multer({
   storage: multer.memoryStorage(),
   limits: {fileSize: 2 * 1024 * 1024,},
   fileFilter: (req,file,cb)=> {
-    const allowedTypes =['image/'];
-
-    if(allowedTypes.includes(file.mimetype)){
+    if(file.mimetype.startsWith('image/')){
       cb(null, true);
     } else {
       cb(new Error('Only images allowed'),false);
